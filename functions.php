6e7d44afb5f7c775516c272ce3d33b126d6ff885
josecaos <jazzvoon@gmail.com>
2018-11-13 12:30:12 -0600

    Font logo + template inicio

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 
 /**
  * Add parent and child stylesheets
@@ -7,8 +7,9 @@
 add_action( 'wp_enqueue_scripts', 'syriah_child_enqueue_styles' );
 if(!function_exists('syriah_child_enqueue_styles')) {
 function syriah_child_enqueue_styles() {
-    wp_enqueue_style( 'syriah-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'syriah-child-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'syriah-logo-font', 'https://fonts.googleapis.com/css?family=Hammersmith+One' );
+    wp_enqueue_style( 'syriah-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style( 'syriah-child-style', get_stylesheet_uri() . '/style.css');
 }}
 
 /**
@@ -18,7 +19,7 @@ add_action( 'after_switch_theme', 'syriah_child_rewrite_flush_child' );
 if(!function_exists('syriah_child_rewrite_flush_child')) {
 function syriah_child_rewrite_flush_child() {
     flush_rewrite_rules();
-}}	
+}}
 
 
 /**
@@ -31,3 +32,5 @@ function syriah_child_theme_setup() {
 	load_child_theme_textdomain( 'syriah-child',  get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'syriah_child_theme_setup' );
+
+// CUSTOM
---
+++ page.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Package: Kentha
+Template Name: Syriah Pagina Inicio
+*/
+
+get_header();
+?>
+<!-- ======================= MAIN SECTION  ======================= -->
+<div id="maincontent">
+	<div class="qt-main qt-clearfix qt-3dfx-content">
+		<?php while ( have_posts() ) : the_post(); ?>
+		<?php get_template_part( 'phpincludes/part-background' ); ?>
+		<div id="qtarticle" <?php post_class("qt-container qt-main-contents"); ?>>
+
+			<!-- <div class="qt-pageheader-std <?php //kentha_is_negative(); ?>">
+				<hr class="qt-spacer-m">
+				<h1 class="qt-caption"><?php //the_title(); ?></h1>
+				<hr class="qt-capseparator">
+			</div> -->
+
+			<?php
+			if( has_post_thumbnail() ){ ?>
+			<a class="qt-imglink qt-card qt-featuredimage" href="<?php the_post_thumbnail_url("full"); ?>">
+				<?php the_post_thumbnail("large" ); ?>
+			</a>
+			<?php } ?>
+			<div class="qt-the-content qt-paper qt-paddedcontent  qt-card">
+				<div class="qt-the-content">
+					<?php the_content(); ?>
+					<hr class="qt-clearfix">
+					<?php wp_link_pages(); ?>
+				</div>
+			</div>
+			<?php comments_template(); ?>
+			<hr class="qt-spacer-l">
+		</div>
+		<?php endwhile; ?>
+	</div>
+</div>
+<!-- ======================= MAIN SECTION END ======================= -->
+<?php get_footer();
